a436993fb074ad02e75de43933a13b9702026314
FahrizalDR <frinaldi@student.ciputra.ac.id>
2022-04-25 10:58:14 +0700

    Add project files.

---
+++ W10Praktikum/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿
+namespace W10Praktikum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LabelCapacity = new System.Windows.Forms.Label();
+            this.OutputCapacity = new System.Windows.Forms.Label();
+            this.OutputStadium = new System.Windows.Forms.Label();
+            this.LabelStadium = new System.Windows.Forms.Label();
+            this.labelVS = new System.Windows.Forms.Label();
+            this.LabelKaptenKanan = new System.Windows.Forms.Label();
+            this.OutputKaptenKanan = new System.Windows.Forms.Label();
+            this.OutputManagerKanan = new System.Windows.Forms.Label();
+            this.LabelManagerKanan = new System.Windows.Forms.Label();
+            this.LabelKaptenKiri = new System.Windows.Forms.Label();
+            this.OutputKaptenKiri = new System.Windows.Forms.Label();
+            this.OutputManagerKiri = new System.Windows.Forms.Label();
+            this.LabelManagerKiri = new System.Windows.Forms.Label();
+            this.ComboBoxKanan = new System.Windows.Forms.ComboBox();
+            this.ComboBoxKiri = new System.Windows.Forms.ComboBox();
+            this.buttonDetailMatch = new System.Windows.Forms.Button();
+            this.labelTanggal = new System.Windows.Forms.Label();
+            this.labelOutputTanggal = new System.Windows.Forms.Label();
+            this.labelOutputSkor = new System.Windows.Forms.Label();
+            this.labelSkor = new System.Windows.Forms.Label();
+            this.DgvDetailMatch = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvDetailMatch)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LabelCapacity
+            // 
+            this.LabelCapacity.AutoSize = true;
+            this.LabelCapacity.Location = new System.Drawing.Point(313, 278);
+            this.LabelCapacity.Name = "LabelCapacity";
+            this.LabelCapacity.Size = new System.Drawing.Size(70, 17);
+            this.LabelCapacity.TabIndex = 29;
+            this.LabelCapacity.Text = "Capacity :";
+            // 
+            // OutputCapacity
+            // 
+            this.OutputCapacity.AutoSize = true;
+            this.OutputCapacity.Location = new System.Drawing.Point(383, 279);
+            this.OutputCapacity.Name = "OutputCapacity";
+            this.OutputCapacity.Size = new System.Drawing.Size(76, 17);
+            this.OutputCapacity.TabIndex = 28;
+            this.OutputCapacity.Text = ".................";
+            // 
+            // OutputStadium
+            // 
+            this.OutputStadium.AutoSize = true;
+            this.OutputStadium.Location = new System.Drawing.Point(383, 240);
+            this.OutputStadium.Name = "OutputStadium";
+            this.OutputStadium.Size = new System.Drawing.Size(76, 17);
+            this.OutputStadium.TabIndex = 27;
+            this.OutputStadium.Text = ".................";
+            // 
+            // LabelStadium
+            // 
+            this.LabelStadium.AutoSize = true;
+            this.LabelStadium.Location = new System.Drawing.Point(313, 239);
+            this.LabelStadium.Name = "LabelStadium";
+            this.LabelStadium.Size = new System.Drawing.Size(67, 17);
+            this.LabelStadium.TabIndex = 26;
+            this.LabelStadium.Text = "Stadium :";
+            // 
+            // labelVS
+            // 
+            this.labelVS.AutoSize = true;
+            this.labelVS.Location = new System.Drawing.Point(380, 90);
+            this.labelVS.Name = "labelVS";
+            this.labelVS.Size = new System.Drawing.Size(26, 17);
+            this.labelVS.TabIndex = 25;
+            this.labelVS.Text = "VS";
+            // 
+            // LabelKaptenKanan
+            // 
+            this.LabelKaptenKanan.AutoSize = true;
+            this.LabelKaptenKanan.Location = new System.Drawing.Point(516, 194);
+            this.LabelKaptenKanan.Name = "LabelKaptenKanan";
+            this.LabelKaptenKanan.Size = new System.Drawing.Size(64, 17);
+            this.LabelKaptenKanan.TabIndex = 24;
+            this.LabelKaptenKanan.Text = "Captain :";
+            // 
+            // OutputKaptenKanan
+            // 
+            this.OutputKaptenKanan.AutoSize = true;
+            this.OutputKaptenKanan.Location = new System.Drawing.Point(586, 195);
+            this.OutputKaptenKanan.Name = "OutputKaptenKanan";
+            this.OutputKaptenKanan.Size = new System.Drawing.Size(76, 17);
+            this.OutputKaptenKanan.TabIndex = 23;
+            this.OutputKaptenKanan.Text = ".................";
+            // 
+            // OutputManagerKanan
+            // 
+            this.OutputManagerKanan.AutoSize = true;
+            this.OutputManagerKanan.Location = new System.Drawing.Point(586, 156);
+            this.OutputManagerKanan.Name = "OutputManagerKanan";
+            this.OutputManagerKanan.Size = new System.Drawing.Size(76, 17);
+            this.OutputManagerKanan.TabIndex = 22;
+            this.OutputManagerKanan.Text = ".................";
+            // 
+            // LabelManagerKanan
+            // 
+            this.LabelManagerKanan.AutoSize = true;
+            this.LabelManagerKanan.Location = new System.Drawing.Point(516, 155);
+            this.LabelManagerKanan.Name = "LabelManagerKanan";
+            this.LabelManagerKanan.Size = new System.Drawing.Size(72, 17);
+            this.LabelManagerKanan.TabIndex = 21;
+            this.LabelManagerKanan.Text = "Manager :";
+            // 
+            // LabelKaptenKiri
+            // 
+            this.LabelKaptenKiri.AutoSize = true;
+            this.LabelKaptenKiri.Location = new System.Drawing.Point(92, 194);
+            this.LabelKaptenKiri.Name = "LabelKaptenKiri";
+            this.LabelKaptenKiri.Size = new System.Drawing.Size(64, 17);
+            this.LabelKaptenKiri.TabIndex = 20;
+            this.LabelKaptenKiri.Text = "Captain :";
+            // 
+            // OutputKaptenKiri
+            // 
+            this.OutputKaptenKiri.AutoSize = true;
+            this.OutputKaptenKiri.Location = new System.Drawing.Point(162, 195);
+            this.OutputKaptenKiri.Name = "OutputKaptenKiri";
+            this.OutputKaptenKiri.Size = new System.Drawing.Size(76, 17);
+            this.OutputKaptenKiri.TabIndex = 19;
+            this.OutputKaptenKiri.Text = ".................";
+            // 
+            // OutputManagerKiri
+            // 
+            this.OutputManagerKiri.AutoSize = true;
+            this.OutputManagerKiri.Location = new System.Drawing.Point(162, 156);
+            this.OutputManagerKiri.Name = "OutputManagerKiri";
+            this.OutputManagerKiri.Size = new System.Drawing.Size(76, 17);
+            this.OutputManagerKiri.TabIndex = 18;
+            this.OutputManagerKiri.Text = ".................";
+            // 
+            // LabelManagerKiri
+            // 
+            this.LabelManagerKiri.AutoSize = true;
+            this.LabelManagerKiri.Location = new System.Drawing.Point(92, 155);
+            this.LabelManagerKiri.Name = "LabelManagerKiri";
+            this.LabelManagerKiri.Size = new System.Drawing.Size(72, 17);
+            this.LabelManagerKiri.TabIndex = 17;
+            this.LabelManagerKiri.Text = "Manager :";
+            // 
+            // ComboBoxKanan
+            // 
+            this.ComboBoxKanan.FormattingEnabled = true;
+            this.ComboBoxKanan.Location = new System.Drawing.Point(513, 87);
+            this.ComboBoxKanan.Name = "ComboBoxKanan";
+            this.ComboBoxKanan.Size = new System.Drawing.Size(195, 24);
+            this.ComboBoxKanan.TabIndex = 16;
+            this.ComboBoxKanan.SelectedIndexChanged += new System.EventHandler(this.ComboBoxKanan_SelectedIndexChanged);
+            // 
+            // ComboBoxKiri
+            // 
+            this.ComboBoxKiri.FormattingEnabled = true;
+            this.ComboBoxKiri.Location = new System.Drawing.Point(95, 87);
+            this.ComboBoxKiri.Name = "ComboBoxKiri";
+            this.ComboBoxKiri.Size = new System.Drawing.Size(195, 24);
+            this.ComboBoxKiri.TabIndex = 15;
+            this.ComboBoxKiri.SelectedIndexChanged += new System.EventHandler(this.ComboBoxKiri_SelectedIndexChanged);
+            // 
+            // buttonDetailMatch
+            // 
+            this.buttonDetailMatch.Location = new System.Drawing.Point(351, 328);
+            this.buttonDetailMatch.Name = "buttonDetailMatch";
+            this.buttonDetailMatch.Size = new System.Drawing.Size(75, 23);
+            this.buttonDetailMatch.TabIndex = 30;
+            this.buttonDetailMatch.Text = "Check";
+            this.buttonDetailMatch.UseVisualStyleBackColor = true;
+            this.buttonDetailMatch.Click += new System.EventHandler(this.buttonDetailMatch_Click);
+            // 
+            // labelTanggal
+            // 
+            this.labelTanggal.AutoSize = true;
+            this.labelTanggal.Location = new System.Drawing.Point(310, 377);
+            this.labelTanggal.Name = "labelTanggal";
+            this.labelTanggal.Size = new System.Drawing.Size(68, 17);
+            this.labelTanggal.TabIndex = 26;
+            this.labelTanggal.Text = "Tanggal :";
+            // 
+            // labelOutputTanggal
+            // 
+            this.labelOutputTanggal.AutoSize = true;
+            this.labelOutputTanggal.Location = new System.Drawing.Point(380, 378);
+            this.labelOutputTanggal.Name = "labelOutputTanggal";
+            this.labelOutputTanggal.Size = new System.Drawing.Size(76, 17);
+            this.labelOutputTanggal.TabIndex = 27;
+            this.labelOutputTanggal.Text = ".................";
+            // 
+            // labelOutputSkor
+            // 
+            this.labelOutputSkor.AutoSize = true;
+            this.labelOutputSkor.Location = new System.Drawing.Point(380, 417);
+            this.labelOutputSkor.Name = "labelOutputSkor";
+            this.labelOutputSkor.Size = new System.Drawing.Size(76, 17);
+            this.labelOutputSkor.TabIndex = 28;
+            this.labelOutputSkor.Text = ".................";
+            // 
+            // labelSkor
+            // 
+            this.labelSkor.AutoSize = true;
+            this.labelSkor.Location = new System.Drawing.Point(313, 417);
+            this.labelSkor.Name = "labelSkor";
+            this.labelSkor.Size = new System.Drawing.Size(65, 17);
+            this.labelSkor.TabIndex = 29;
+            this.labelSkor.Text = "Skor      :";
+            // 
+            // DgvDetailMatch
+            // 
+            this.DgvDetailMatch.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvDetailMatch.Location = new System.Drawing.Point(12, 466);
+            this.DgvDetailMatch.Name = "DgvDetailMatch";
+            this.DgvDetailMatch.RowHeadersWidth = 51;
+            this.DgvDetailMatch.RowTemplate.Height = 24;
+            this.DgvDetailMatch.Size = new System.Drawing.Size(776, 231);
+            this.DgvDetailMatch.TabIndex = 31;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 709);
+            this.Controls.Add(this.DgvDetailMatch);
+            this.Controls.Add(this.buttonDetailMatch);
+            this.Controls.Add(this.labelSkor);
+            this.Controls.Add(this.LabelCapacity);
+            this.Controls.Add(this.labelOutputSkor);
+            this.Controls.Add(this.OutputCapacity);
+            this.Controls.Add(this.labelOutputTanggal);
+            this.Controls.Add(this.OutputStadium);
+            this.Controls.Add(this.labelTanggal);
+            this.Controls.Add(this.LabelStadium);
+            this.Controls.Add(this.labelVS);
+            this.Controls.Add(this.LabelKaptenKanan);
+            this.Controls.Add(this.OutputKaptenKanan);
+            this.Controls.Add(this.OutputManagerKanan);
+            this.Controls.Add(this.LabelManagerKanan);
+            this.Controls.Add(this.LabelKaptenKiri);
+            this.Controls.Add(this.OutputKaptenKiri);
+            this.Controls.Add(this.OutputManagerKiri);
+            this.Controls.Add(this.LabelManagerKiri);
+            this.Controls.Add(this.ComboBoxKanan);
+            this.Controls.Add(this.ComboBoxKiri);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvDetailMatch)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelCapacity;
+        private System.Windows.Forms.Label OutputCapacity;
+        private System.Windows.Forms.Label OutputStadium;
+        private System.Windows.Forms.Label LabelStadium;
+        private System.Windows.Forms.Label labelVS;
+        private System.Windows.Forms.Label LabelKaptenKanan;
+        private System.Windows.Forms.Label OutputKaptenKanan;
+        private System.Windows.Forms.Label OutputManagerKanan;
+        private System.Windows.Forms.Label LabelManagerKanan;
+        private System.Windows.Forms.Label LabelKaptenKiri;
+        private System.Windows.Forms.Label OutputKaptenKiri;
+        private System.Windows.Forms.Label OutputManagerKiri;
+        private System.Windows.Forms.Label LabelManagerKiri;
+        private System.Windows.Forms.ComboBox ComboBoxKanan;
+        private System.Windows.Forms.ComboBox ComboBoxKiri;
+        private System.Windows.Forms.Button buttonDetailMatch;
+        private System.Windows.Forms.Label labelTanggal;
+        private System.Windows.Forms.Label labelOutputTanggal;
+        private System.Windows.Forms.Label labelOutputSkor;
+        private System.Windows.Forms.Label labelSkor;
+        private System.Windows.Forms.DataGridView DgvDetailMatch;
+    }
+}
+
---
+++ W10Praktikum/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace W10Praktikum
+{
+    public partial class Form1 : Form
+    {
+        public static string SqlConnection = "server = localhost; uid = root; pwd =; database = premier_league";
+        public MySqlConnection sqlConnect = new MySqlConnection(SqlConnection);
+        public MySqlCommand sqlCommand;
+        public MySqlDataAdapter sqlAdapter;
+        public string sqlQuery;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dataTeamKiri = new DataTable();
+            sqlQuery = "SELECT team_name as 'Team Name', team_id as 'ID Team' FROM team";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dataTeamKiri);
+            ComboBoxKiri.DisplayMember = "Team Name";
+            ComboBoxKiri.ValueMember = "ID Team";
+            ComboBoxKiri.DataSource = dataTeamKiri;
+
+            DataTable dataTeamKanan = new DataTable();
+            sqlQuery = "SELECT team_name as 'Team Name', team_id as 'ID Team' FROM team";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dataTeamKanan);
+            ComboBoxKanan.DisplayMember = "Team Name";
+            ComboBoxKanan.ValueMember = "ID Team";
+            ComboBoxKanan.DataSource = dataTeamKanan;
+        }
+        
+        private void ComboBoxKiri_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable dataManagerDanCaptainKiri = new DataTable();
+            sqlQuery = "SELECT manager.manager_name as `Manager Name`, player.player_name as `Captain Name` FROM team, player, manager WHERE team.captain_id = player.player_id AND manager.manager_id = team.manager_id AND team.team_id = '" + ComboBoxKiri.SelectedValue.ToString() + "'";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dataManagerDanCaptainKiri);
+            OutputManagerKiri.Text = dataManagerDanCaptainKiri.Rows[0]["Manager Name"].ToString();
+            OutputKaptenKiri.Text = dataManagerDanCaptainKiri.Rows[0]["Captain Name"].ToString();
+        }
+
+        private void ComboBoxKanan_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable dataManagerDanCaptainKanan = new DataTable();
+            sqlQuery = "SELECT manager.manager_name as `Manager Name`, player.player_name as `Captain Name` FROM team, player, manager WHERE team.captain_id = player.player_id AND manager.manager_id = team.manager_id AND team.team_id = '" + ComboBoxKanan.SelectedValue.ToString() + "'";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(dataManagerDanCaptainKanan);
+            OutputManagerKanan.Text = dataManagerDanCaptainKanan.Rows[0]["Manager Name"].ToString();
+            OutputKaptenKanan.Text = dataManagerDanCaptainKanan.Rows[0]["Captain Name"].ToString();
+
+            DataTable Stadium = new DataTable();
+            sqlQuery = "SELECT concat(home_stadium, ', ',team.city) as `Stadium`, capacity as `Capacity` FROM team WHERE team_id = '" + ComboBoxKiri.SelectedValue.ToString() + "'";
+            sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            sqlAdapter.Fill(Stadium);
+            OutputStadium.Text = Stadium.Rows[0]["Stadium"].ToString();
+            OutputCapacity.Text = Stadium.Rows[0]["Capacity"].ToString();
+        }
+
+        private void buttonDetailMatch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable TanggalDanSKor = new DataTable();
+                sqlQuery = "select date_format(match_date, '%e %M %Y') as `Tanggal`, concat(goal_home, '-', goal_away) as `Skor` from `match` where team_home = '" + ComboBoxKiri.SelectedValue.ToString() + "' and team_away = '" + ComboBoxKanan.SelectedValue.ToString() + "'";
+                sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+                sqlAdapter = new MySqlDataAdapter(sqlCommand);
+                sqlAdapter.Fill(TanggalDanSKor);
+                labelOutputTanggal.Text = TanggalDanSKor.Rows[0]["Tanggal"].ToString();
+                labelOutputSkor.Text = TanggalDanSKor.Rows[0]["Skor"].ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Pertandingan antara kedua tim tidak ada!");                
+            }
+
+            //DataTable DetailMatch = new DataTable();
+            //sqlQuery = "";
+            //sqlCommand = new MySqlCommand(sqlQuery, sqlConnect);
+            //sqlAdapter = new MySqlDataAdapter(sqlCommand);
+            //sqlAdapter.Fill(DetailMatch);
+            //DgvDetailMatch.DataSource = DetailMatch;
+
+        }
+    }
+}
